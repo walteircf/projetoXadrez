cc4249d9ff3c3b5c5ac5db1ff7842e9e112ce820
Walteir Coelho <wcfreitasf@gmail.com>
2024-05-21 21:17:51 -0300

    Começando a implementar o tabuleiro

--- ChessProject-Console/Program.cs
+++ ChessProject-Console/Program.cs
@@ -9,11 +9,10 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao P;
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            P = new Posicao(3, 4);
 
-            Console.WriteLine("Posição: " + P);
+
             Console.ReadLine();
         }
     }
