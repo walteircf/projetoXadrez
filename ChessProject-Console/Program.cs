f3492c70d84f8c05125dbd31c074ef289d3f5f01
Walteir Coelho <wcfreitasf@gmail.com>
2024-05-27 19:40:54 -0300

    Começando a colocar peças no tabuleiro

--- ChessProject-Console/Program.cs
+++ ChessProject-Console/Program.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.Collections.Generic;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -10,6 +11,10 @@ namespace xadrez_console
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new  Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
             Tela.imprimirTabuleiro(tab);
 
